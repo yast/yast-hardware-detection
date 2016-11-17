fb479ba3df979cdd31c9396f0fbd067a39b84112
Steffen Winterfeldt <wfeldt@opensuse.org>
2016-11-17 13:37:57 +0100

    don't require new libhd but check feature during compile time

--- agent/HwParse.cc
+++ agent/HwParse.cc
@@ -338,6 +338,7 @@ HwProbe::resource_type2map (const res_any_t *res, const char **name)
 	map->add (YCPString ("addr"), YCPString (r->addr));
     }
     break;
+#if HD_FULL_VERSION >= 21036
     case res_phwaddr:
     {
 	RES2TYPE (res_hwaddr_t);
@@ -345,6 +346,7 @@ HwProbe::resource_type2map (const res_any_t *res, const char **name)
 	map->add (YCPString ("addr"), YCPString (r->addr));
     }
     break;
+#endif
     case res_link:
     {
 	RES2TYPE (res_link_t);
